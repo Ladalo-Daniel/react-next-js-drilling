7628a736de9d672a3a7000347d4628b8221151dc
Ladalo-Daniel <balaladalo@gmail.com>
2024-02-14 19:07:45 +0100

    home-page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,10 @@
+// import Whatsapp from "@/components/Whatsapp";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
        <h1>Home Page</h1>
+       {/* <Whatsapp /> */}
     </main>
   );
 }
---
+++ src/components/Whatsapp.jsx
@@ -0,0 +1,14 @@
+"use client"
+
+import React from 'react'
+import { FloatingWhatsApp } from 'react-floating-whatsapp'
+
+function Whatsapp() {
+  return (
+    <div>
+        <FloatingWhatsApp phoneNumber='+2349154029723'  />
+    </div>
+  )
+}
+
+export default Whatsapp
